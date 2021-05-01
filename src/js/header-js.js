b2d45ce6dd714f6de921aaa7a1bab155ae0d78c3
AntonLovkin <anton28111987@gmail.com>
2021-05-01 11:27:25 +0300

    start footer

--- src/js/header-js.js
+++ src/js/header-js.js
@@ -1,6 +1,12 @@
 
-const headerBtnRef = document.querySelector('.navigation-button');
+const headerBtnRef = document.querySelector('.button');
 
 headerBtnRef.addEventListener('click', () => {
     alert('Authorized here')
+})
+
+const searchBtn = document.querySelector('#search-btn');
+
+searchBtn.addEventListener('click', () => {
+    alert('Search')
 })
